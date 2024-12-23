86d3792e31ed6f7559161ec36168c0aa6c7a92bf
Onup Chandra Barmon <43741354+onuproy@users.noreply.github.com>
2024-12-23 23:46:49 +0600

    plugin update

--- wp-content/plugins/my-new-plugin/my-new-plugin.php
+++ wp-content/plugins/my-new-plugin/my-new-plugin.php
@@ -13,28 +13,26 @@
  * 
 */
 
+// SingleTon patten 
 
-// Singleton Pattern
-
-class Sr_my_first_plugin{
+class First_my_plugin{
 
     private static $instance;
-
     private function __construct(){
-        add_filter ('the_content', array( $this, 'the_content_callback' ));
-
+         add_filter ('the_content', array( $this, 'the_content_callback' ));
     }
 
     public static function get_instance(){
-        if( self::$instance ){
+        if(self::$instance){
             return self::$instance;
         }
+
         self::$instance = new self();
 
         return self::$instance;
     }
-    
-    public function the_content_callback( $content ){
+
+    public  function the_content_callback( $content ){
 
             $url = get_the_permalink();
 
@@ -50,8 +48,49 @@ class Sr_my_first_plugin{
 
 
 }
+First_my_plugin::get_instance();
 
-Sr_my_first_plugin::get_instance();
+
+
+
+
+
+
+// Singleton Pattern
+// class Sr_my_first_plugin{
+
+//     private static $instance;
+
+//     private function __construct(){
+//         add_filter ('the_content', array( $this, 'the_content_callback' ));
+
+//     }
+
+//     public static function get_instance(){
+//         if( self::$instance ){
+//             return self::$instance;
+//         }
+//         self::$instance = new self();
+
+//         return self::$instance;
+//     }
+    
+//     public function the_content_callback( $content ){
+
+//             $url = get_the_permalink();
+
+//             $image = '<p> <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $url . '"></p>';
+//             $content .=$image;
+//             // return 'My new content 2';
+//             // return $content . '<p>My new content</p>';
+
+//             return $content;
+
+//         }
+
+// }
+
+// Sr_my_first_plugin::get_instance();
 
 // new Sr_my_first_plugin();
 
